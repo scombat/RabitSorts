1759d417ea7da109d775aff72aae917303edc764
scombat <scombat@student.42.fr>
2014-09-28 21:56:32 +0200

    add al, de, ca, fr alphabets defines

--- includes/rabitsorts.h
+++ includes/rabitsorts.h
@@ -6,7 +6,7 @@
 /*   By: scombat <scombat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/28 21:19:27 by scombat           #+#    #+#             */
-/*   Updated: 2014/09/28 21:20:45 by scombat          ###   ########.fr       */
+/*   Updated: 2014/09/28 21:53:11 by scombat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,20 @@
 /*
 **		defines
 */
+# define		ALPHABET_L		B_ALPH_L_FR
+# define		ALPHABET_U		B_ALPH_U_FR
+
+# define		B_ALPH_L_AL		"aâbcçdeëfghijklmnopqrstuvwxyz"
+# define		B_ALPH_U_AL		"AÂBCÇDEËFGHIJKLMNOPQRSTUVWXYZ"
+
+# define		B_ALPH_L_DE		"abcdefghijklmnopqrstuvwxyzäöü"
+# define		B_ALPH_U_DE		"ABCDEFGHIJKLMNOPQRSTUVWXYZßÄöü"
+
+# define		B_ALPH_L_CA		"aáàbcçdeéèfghiíïjklmnoóòpqrstuúvwxyz"
+# define		B_ALPH_U_CA		"ABCÇDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# define		B_ALPH_L_FR		"AÀÂBCÇDEÉÊĒÈFGHIÏÎJKLMNOÔŒPQRSTUÙÛVWXYZ"
+# define		B_ALPH_U_FR		"aàâbcçdeéêēèfghiïîjklmnoôœpqrstuùûvwxyz"
+
 
 #endif
