2241a4639c3bae484d5769ed7c21dc7cc310b7ce
scombat <scombat@student.42.fr>
2014-10-12 03:00:22 +0200

    add support of personnal base and support order type ASC / DESC

--- src/cmp_c.c
+++ src/cmp_c.c
@@ -6,14 +6,22 @@
 /*   By: scombat <scombat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/30 09:01:20 by scombat           #+#    #+#             */
-/*   Updated: 2014/09/30 09:34:17 by scombat          ###   ########.fr       */
+/*   Updated: 2014/10/12 01:22:01 by scombat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rabitsorts.h"
 
 bool	cmp_c(char c1, char c2) {
+	t_sortopts		*opt;
+	bool			order;
+
+	opt = get_opt();
+	order = opt->order;
 	if (c1 == c2)
 		return (true);
-	return ((c1 < c2) ? ((ORDER) ? true: false): ((ORDER) ? false: true));
+	if (opt->base)
+		return ((pos_c_base(c1) < pos_c_base(c2)) ? ((order) ? true: false):\
+		 ((order) ? false: true));
+	return ((c1 < c2) ? ((order) ? true: false): ((order) ? false: true));
 }
--- src/cmp_int.c
+++ src/cmp_int.c
@@ -6,14 +6,30 @@
 /*   By: scombat <scombat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/30 09:34:53 by scombat           #+#    #+#             */
-/*   Updated: 2014/09/30 09:35:39 by scombat          ###   ########.fr       */
+/*   Updated: 2014/10/01 08:47:20 by scombat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rabitsorts.h"
 
 bool	cmp_int(int i1, int i2) {
+	t_sortopts		*opt;
+	bool			order;
+
+	opt = get_opt();
+	order = opt->order;
 	if (i1 == i2)
 		return (true);
-	return ((i1 < i2) ? ((ORDER) ? true: false): ((ORDER) ? false: true));
+	if (order == O_ASC) {
+		if (i1 > i2)
+			return (false);
+		else
+			return (true);
+	} else if (order == O_DESC) {
+		if (i1 < i2)
+			return (false);
+		else
+			return (true);
+	}
+	return (false);
 }
